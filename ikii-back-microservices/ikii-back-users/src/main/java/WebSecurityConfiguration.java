3d21f041ec8404f354c6a18d5dda54a2f1883aef
Arturo <arturovelazquez94@gmail.com>
2020-05-23 19:22:42 -0500

    feature Resets pom version in microservices

--- ikii-back-microservices/ikii-back-users/src/main/java/WebSecurityConfiguration.java
+++ ikii-back-microservices/ikii-back-users/src/main/java/WebSecurityConfiguration.java
@@ -1,7 +1,6 @@
-package mx.ikii.users;
+
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -19,8 +18,7 @@ import mx.ikii.commons.security.CustomUserDetailsService;
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
-	@Value("${custom-security.signingKey}")
-	private String signingKey;
+	private static final String SIGNING_KEY = "s1f41234pwqdqkl4l12ghg9853123sd";
 
 	@Autowired
 	private CustomUserDetailsService customUserDetailsService;
@@ -30,7 +28,7 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
 	@Override
 	public void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.userDetailsService(customUserDetailsService);
+		auth.userDetailsService(customUserDetailsService); 
 		auth.authenticationProvider(accountAuthenticationProvider);
 	}
 
@@ -43,7 +41,7 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 	@Bean
 	public JwtAccessTokenConverter jwtAccessTokenConverter() {
 		JwtAccessTokenConverter jwtAccessTokenConverter = new JwtAccessTokenConverter();
-		jwtAccessTokenConverter.setSigningKey(signingKey);
+		jwtAccessTokenConverter.setSigningKey(SIGNING_KEY);
 		return jwtAccessTokenConverter;
 	}
 
--- ikii-back-microservices/ikii-back-users/src/main/java/mx/ikii/users/controller/IUserClipController.java
+++ ikii-back-microservices/ikii-back-users/src/main/java/mx/ikii/users/controller/IUserClipController.java
@@ -3,7 +3,6 @@ package mx.ikii.users.controller;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -51,7 +50,6 @@ public interface IUserClipController {
 	 * @return the HTTP DTO response wrapping the users
 	 */
 	@GetMapping
-	@PreAuthorize("hasAuthority('PRIVILEGE_ADMIN_READ')")
 	ResponseEntity<Page<UserClipResponse>> getAll(Pageable pageable);
 
 	/**
