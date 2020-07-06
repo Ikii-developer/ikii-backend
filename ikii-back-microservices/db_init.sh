ba13f2d105dc339ae64723f22d0e8256960ae0f9
AlfonsoTovar <a_tovar_s@hotmail.com>
2020-07-06 17:06:36 -0500

    user and password

--- ikii-back-microservices/db_init.sh
+++ ikii-back-microservices/db_init.sh
@@ -2,7 +2,7 @@
 																																																																								MONGO_PORT=$1
 
 # MONGO RESTORE
-cd dbinit && mongorestore --host 127.0.0.1 --port $MONGO_PORT --db ikiidb
+cd dbinit && mongorestore --host 127.0.0.1 --port $MONGO_PORT --db ikiidb --username=ikii@dm1ndB --password=pass1Ki1Db --authenticationDatabase=ikiidb
 
 echo "Admin users created."
 
