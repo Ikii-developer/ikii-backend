ad2c8b0c2e3d484e360d8f3950bf805d104b8750
Jorge Luis Rosales Sanchez <jorgerosales.rs@gmail.com>
2020-08-11 15:01:34 -0500

    making change that where requested on the PR

--- ikii-back-commons/src/main/java/mx/ikii/commons/payload/request/business/BusinessRateRequest.java
+++ ikii-back-commons/src/main/java/mx/ikii/commons/payload/request/business/BusinessRateRequest.java
@@ -10,15 +10,10 @@ import lombok.Data;
 public class BusinessRateRequest implements Serializable{
 	
 	private static final long serialVersionUID = -1339140897440540001L;
-
 	private String id;
-	
 	private String businessId;
-	
 	private LocalDateTime updatedAt;
-	
 	private Double average;
-	
 	private List<RateRequest> rates;
 
 }
