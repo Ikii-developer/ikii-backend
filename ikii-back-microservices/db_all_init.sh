b2f8b9c4bacd947e4ad03742cd801a7920000bf4
AlfonsoTovar <a_tovar_s@hotmail.com>
2020-08-15 05:20:38 -0500

    scripts sample data

--- ikii-back-microservices/db_all_init.sh
+++ ikii-back-microservices/db_all_init.sh
@@ -2,22 +2,27 @@
 
 #The order of the scripts executions is important	
 																			
-# Execute users restore
+echo "**************** Execute users restore ****************"
 sh ./db_users_init.sh
 
-# Execute business restore
+
+echo "**************** Execute business restore ****************"
 sh ./db_business_init.sh
 
-# Execute business-category restore
-sh ./db_business_category_init.sh
 
-# Execute business-rate restore
+echo "**************** Execute business_categories restore ****************"
+sh ./db_business_categories_init.sh
+
+
+echo "**************** Execute business-rate restore ****************"
 sh ./db_business_rate_init.sh
 
-# Execute categories restore
+
+echo "**************** Execute categories restore ****************"
 sh ./db_categories_init.sh
 
-# Execute customer-address restore
+
+echo "**************** Execute customer-address restore ****************"
 sh ./db_customer_address.sh
 
 
