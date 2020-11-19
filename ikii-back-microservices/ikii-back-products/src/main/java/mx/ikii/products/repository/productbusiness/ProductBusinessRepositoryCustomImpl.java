8180ec25f31fbd7ad36c88dee6653edcee44556f
arturo-velazquez-vargas <alfonso.ts100@gmail.com>
2020-11-19 02:26:40 -0600

    add deliveryRange to Business
    refact code

--- ikii-back-microservices/ikii-back-products/src/main/java/mx/ikii/products/repository/productbusiness/ProductBusinessRepositoryCustomImpl.java
+++ ikii-back-microservices/ikii-back-products/src/main/java/mx/ikii/products/repository/productbusiness/ProductBusinessRepositoryCustomImpl.java
@@ -2,18 +2,15 @@ package mx.ikii.products.repository.productbusiness;
 
 import java.util.ArrayList;
 import java.util.List;
-import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.aggregation.Aggregation;
 import org.springframework.data.mongodb.core.aggregation.AggregationOperation;
 import org.springframework.data.mongodb.core.aggregation.MatchOperation;
-import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.TextCriteria;
 import org.springframework.stereotype.Repository;
 import mx.ikii.commons.payload.request.product.ProductFilter;
 import mx.ikii.commons.persistence.collection.ProductBusiness;
-import mx.ikii.commons.utils.Nullable;
 
 @Repository
 public class ProductBusinessRepositoryCustomImpl implements IProductBusinessRepositoryCustom {
@@ -28,13 +25,6 @@ public class ProductBusinessRepositoryCustomImpl implements IProductBusinessRepo
     TextCriteria criteria = TextCriteria.forLanguage("es").matchingAny(productFilter.getKeywords());
     MatchOperation match = Aggregation.match(criteria);
     aggregations.add(match);
-
-    MatchOperation matchBusiness = null;
-    if (!Nullable.isNullOrEmpty(productFilter.getProductCategoryId())) {
-      matchBusiness = Aggregation.match(Criteria.where("productCategoryId")
-          .is(new ObjectId(productFilter.getProductCategoryId())));
-      aggregations.add(matchBusiness);
-    }
 
     Aggregation aggregation = Aggregation.newAggregation(aggregations);
 
